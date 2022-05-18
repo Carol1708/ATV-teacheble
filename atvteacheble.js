ffa6a5d6a6a0d4fdc6ecfc4b4a8d4f827bed7142
carol1708 <carolqueiroz96@outlook.com.br>
2022-05-17 23:03:10 -0300

    Atividades do Teachable

---
+++ atvteacheble.js
@@ -0,0 +1,42 @@
+/* document.write('<h3>Utilizando utilizando estruturas de repetição, elabore um script para verificar entre 3 números, qual o maior.</h3>')
+
+let n1 = parseFloat(prompt('Digite o primeiro valor'))
+let n2 = parseFloat(prompt('Digite o segundo valor'))
+let n3 = parseFloat(prompt('Digite o terceiro valor'))
+
+if (n1 > n2 && n1 > n3) {
+    alert('O maior valor é: ' + n1)
+}
+else if (n2 > n1 && n2 > n3) {
+    alert('O maior valor é: ' + n2)
+}
+else {
+    alert('O maior valor é: ' + n3)
+} 
+
+document.write('<h3> elabore um script para indicar números positivos, negativos e nulo.</h3>')
+
+let n1 = parseFloat(prompt('Digite um número'))
+
+if (n1 > 0) {
+    alert( n1 + ' é um número positivo ')
+}
+else if (n1 < 0) {
+    alert( n1 + ' é um número negativo ')
+}
+else {
+    alert( n1 + ' é um número nulo ')
+} 
+
+document.write('<h3> Em estrutura de repetição, elabore um script para verificar se um número é par ou ímpar.</h3>')
+
+let n1 = parseFloat(prompt('Digite um número'))
+
+if (n1 % 2 == 0) {
+    alert( n1 + ' é um número par')
+}
+else {
+    alert( n1 + ' é um número ímpar ')
+}  */
+
+document.write('<h3>Utilizando oog comando switch, elabore um script com 5 opções para escolha entre linguagens de programação.</h3>')
